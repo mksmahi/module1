41f559fc6cda84cd866e85b4a9e75af754a68038
Mahesh Kumar Saini <49157369+mksmahi@users.noreply.github.com>
2020-08-17 15:02:43 +0530

    2nd commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
         $scope.message = "";
         $scope.checkLunch = function(){
             var items = $scope.lunchitems.split(",");
-            if(items.length<1) { $scope.message = ""; }
+            if(items.length<1) { $scope.message = "Please Enter Data First"; }
             else if(items.length<=3){ $scope.message ="Enjoy!"; }
             else { $scope.message = "Too much"; }
         }
